032645a5bb46dc47b858340df951c7dc5724d1df
diejofre <diego-jofre@live.com.ar>
2023-01-10 01:30:26 -0300

    base project

--- 12/db/firebase-config.js
+++ 12/db/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+  apiKey: "AIzaSyDoM4lAFy52m4UsbmJji_uCtQbTQnKU57k",
+  authDomain: "ecommerce-b278e.firebaseapp.com",
+  projectId: "ecommerce-b278e",
+  storageBucket: "ecommerce-b278e.appspot.com",
+  messagingSenderId: "840212656823",
+  appId: "1:840212656823:web:ad361615cdb90b93109332",
 };
 
 const app = initializeApp(firebaseConfig);
